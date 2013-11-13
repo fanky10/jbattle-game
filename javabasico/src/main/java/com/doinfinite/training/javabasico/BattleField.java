9b9caedb5952cb5b43c62c94f92afe168d2061f7
Ewen Mackenzie <fanky10@gmail.com>
2013-11-13 19:13:44 -0300

    units cannot attack all other units, restrictions added

--- javabasico/src/main/java/com/doinfinite/training/javabasico/BattleField.java
+++ javabasico/src/main/java/com/doinfinite/training/javabasico/BattleField.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 import com.doinfinite.training.javabasico.entidades.HeavyTank;
 import com.doinfinite.training.javabasico.entidades.LightTank;
-import com.doinfinite.training.javabasico.entidades.AttackableUnit;
+import com.doinfinite.training.javabasico.entidades.Unit;
 import com.doinfinite.training.javabasico.entidades.QuickTank;
 
 public class BattleField {
-	private List<AttackableUnit> redTeam;
-	private List<AttackableUnit> blueTeam;
+	private List<Unit> redTeam;
+	private List<Unit> blueTeam;
 
-	public BattleField(List<AttackableUnit> redTeam, List<AttackableUnit> blueTeam) {
+	public BattleField(List<Unit> redTeam, List<Unit> blueTeam) {
 		super();
 		this.redTeam = redTeam;
 		this.blueTeam = blueTeam;
 	}
-	
+
 	public void battle() {
 		boolean winner = false;
 		while (!winner) {
@@ -46,55 +46,61 @@ public class BattleField {
 		teamAttack(redTeam, blueTeam);
 	}
 
-	private void teamAttack(List<AttackableUnit> attackers,
-			List<AttackableUnit> defenders) {
-
-		// red team goes first
-		for (AttackableUnit attacker : attackers) {
-			Iterator<AttackableUnit> i = defenders.iterator();
-			while (i.hasNext()) {
-				AttackableUnit defender = i.next();
+	private void teamAttack(List<Unit> attackers, List<Unit> defenders) {
+		for (Unit attacker : attackers) {
+			System.out.println(attacker.getClass().getSimpleName()
+					+ " is attacking!!");
+			Iterator<Unit> unit = defenders.iterator();
+			while (unit.hasNext()) {
+				Unit defender = unit.next();
+				if (!attacker.canAttack(defender)) {
+					System.out.println(attacker.getClass().getSimpleName()
+							+ " cannot attack "
+							+ defender.getClass().getSimpleName());
+					continue; //next!
+				}
 				int attack = attacker.ataca();
 				defender.defiende(attack);
-				System.out.println("Defender recibe daño y "+defender);
+				System.out.println(defender.getClass().getSimpleName()
+						+ " gets damage and is left with: "
+						+ defender.getHealth());
 				// depending on health remove it
-				if(defender.isDead()){
-					System.out.println("Ha habido una baja!!");
-					i.remove();
+				if (defender.isDead()) {
+					System.out.println("Unit down!!!");
+					unit.remove();
 				}
 			}
 		}
 	}
-	
-	public static void main(String args[]){
-		List<AttackableUnit> blueTeam = new ArrayList<AttackableUnit>();
+
+	public static void main(String args[]) {
+		List<Unit> blueTeam = new ArrayList<Unit>();
 		blueTeam.add(new HeavyTank(100));
 		blueTeam.add(new LightTank(100));
 		blueTeam.add(new LightTank(100));
-		
-		List<AttackableUnit> redTeam = new ArrayList<AttackableUnit>();
+
+		List<Unit> redTeam = new ArrayList<Unit>();
 		redTeam.add(new HeavyTank(100));
 		redTeam.add(new HeavyTank(100));
 		redTeam.add(new QuickTank(100));
-		
+
 		new BattleField(redTeam, blueTeam).battle();
-		
+
 	}
 
-
-	public List<AttackableUnit> getRedTeam() {
+	public List<Unit> getRedTeam() {
 		return redTeam;
 	}
 
-	public void setRedTeam(List<AttackableUnit> redTeam) {
+	public void setRedTeam(List<Unit> redTeam) {
 		this.redTeam = redTeam;
 	}
 
-	public List<AttackableUnit> getBlueTeam() {
+	public List<Unit> getBlueTeam() {
 		return blueTeam;
 	}
 
-	public void setBlueTeam(List<AttackableUnit> blueTeam) {
+	public void setBlueTeam(List<Unit> blueTeam) {
 		this.blueTeam = blueTeam;
 	}
 

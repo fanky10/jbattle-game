63acae910f0b91390a5f2e67a91de38e7ffda388
Ewen Mackenzie <fanky10@gmail.com>
2014-01-07 12:17:53 -0300

    test for battlefield updated
    now it’s using the corresponding repository instead of the MockedUnits
    static class

--- dev/AppModel/src/test/java/com/doinfinite/battlegame/model/BattlefieldTest.java
+++ dev/AppModel/src/test/java/com/doinfinite/battlegame/model/BattlefieldTest.java
@@ -3,49 +3,52 @@ package com.doinfinite.battlegame.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.doinfinite.battlegame.model.Unit.UnitType;
+import com.doinfinite.battlegame.model.repository.UnitsRepository;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/dataAccessContext.xml" })
+@Transactional
 public class BattlefieldTest extends TestCase {
-	/**
-	 * Create the test case
-	 * 
-	 * @param testName
-	 *            name of the test case
-	 */
-	public BattlefieldTest(String testName) {
-		super(testName);
-	}
 
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(BattlefieldTest.class);
-	}
+	@Autowired
+	private UnitsRepository unitsRepository;
 
-	/**
-	 * Rigourous Test :-)
-	 */
+	@Test
 	public void testHeavyTeam() {
+		List<Unit> units = getUnitsRepository().findAll();
+
 		List<Unit> navyTeam = new ArrayList<Unit>();
-		navyTeam.add(new Unit("Ship", UnitType.WATER, 100, 50, 10, 70, 20));
 		List<Unit> heavyTeam = new ArrayList<Unit>();
-		heavyTeam.add(new Unit("Airplane", UnitType.AIR, 100, 50, 10, 70, 20));
-		heavyTeam.add(new Unit("Apache Chopper", UnitType.AIR, 100, 50, 10, 70,
-				20));
-		heavyTeam
-				.add(new Unit("QuickTank", UnitType.EARTH, 100, 50, 10, 70, 20));
-		heavyTeam
-				.add(new Unit("LightTank", UnitType.EARTH, 100, 50, 10, 70, 20));
-		heavyTeam.add(new Unit("Troop", UnitType.EARTH, 100, 50, 10, 70, 20));
-		heavyTeam.add(new Unit("Turret", UnitType.EARTH, 100, 50, 10, 70, 20));
+		for (Unit u : units) {
+			if (u.getUnitType() == UnitType.WATER) {
+				navyTeam.add(u);
+			}
+			if (u.getUnitType() == UnitType.EARTH
+					|| u.getUnitType() == UnitType.AIR) {
+				heavyTeam.add(u);
+			}
+		}
 		List<BattleEvent> battleEvents = new Battlefield(heavyTeam, navyTeam)
 				.battle();
 		assertTrue(battleEvents != null && !battleEvents.isEmpty()
 				&& battleEvents.size() > 1);
+	}
+
+	public UnitsRepository getUnitsRepository() {
+		return unitsRepository;
+	}
+
+	public void setUnitsRepository(UnitsRepository unitsRepository) {
+		this.unitsRepository = unitsRepository;
 	}
 }

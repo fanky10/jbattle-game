e32d8c726bbf6b412c6910a435b31e63a4a8918c
VictorBurani <buranivictor@gmail.com>
2014-01-11 23:23:59 -0300

    Added getters and setters on BattlefieldType.

--- dev/AppModel/src/main/java/com/doinfinite/battlegame/model/BattlefieldType.java
+++ dev/AppModel/src/main/java/com/doinfinite/battlegame/model/BattlefieldType.java
@@ -11,6 +11,18 @@ public class BattlefieldType {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "battlefield_name", nullable = false)
 	private Integer battlefieldName;
+	public Integer getBattlefieldName() {
+		return battlefieldName;
+	}
+	public void setBattlefieldName(Integer battlefieldName) {
+		this.battlefieldName = battlefieldName;
+	}
+	public String getBattlefieldId() {
+		return battlefieldId;
+	}
+	public void setBattlefieldId(String battlefieldId) {
+		this.battlefieldId = battlefieldId;
+	}
 	@Column(name = "battlefield_id", nullable = false)
 	private String battlefieldId;
 

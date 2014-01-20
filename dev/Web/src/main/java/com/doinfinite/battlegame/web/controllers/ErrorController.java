05f199254e7ef3888925f2c93e793958bcfd4ba9
Ewen Mackenzie <fanky10@gmail.com>
2014-01-20 14:25:00 -0300

    profiling for facebook prod and dev keys

--- dev/Web/src/main/java/com/doinfinite/battlegame/web/controllers/ErrorController.java
+++ dev/Web/src/main/java/com/doinfinite/battlegame/web/controllers/ErrorController.java
@@ -1,10 +1,15 @@
 package com.doinfinite.battlegame.web.controllers;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.doinfinite.battlegame.web.constants.WebAppConstants;
@@ -12,13 +17,18 @@ import com.doinfinite.battlegame.web.constants.WebAppConstants;
 @Controller
 public class ErrorController extends BaseController {
 
+	@Value("${application.traceExceptions}")
+	private Boolean traceExceptions;
+
 	@RequestMapping(value = "/error")
-	public String showErrorPage(HttpServletRequest request,
-			HttpServletResponse response, ModelMap map) throws Exception {
+	@ExceptionHandler(Exception.class)
+	public String showErrorPage(Exception exception, HttpServletRequest request, HttpServletResponse response,
+			ModelMap map) throws Exception {
 		Integer statusCode = getStatusCode(request);
 		response.setStatus(statusCode);
+		String exceptionTrace = exception.getClass() + " Message: " + exception.getMessage() + " - " + getStackTrace(exception);
 		String title = "Application Error";
-		String label = "Unexpected Error";
+		String label = "Unexpected Error" + (traceExceptions ? exceptionTrace : "");
 		String body = "We cannot process your request at the moment";
 		if (statusCode == 404) {
 			title = "Page not found";
@@ -31,5 +41,12 @@ public class ErrorController extends BaseController {
 		map.addAttribute("statusCode", statusCode);
 
 		return WebAppConstants.STATIC_ERROR_PAGE;
+	}
+
+	private String getStackTrace(Throwable t) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		return sw.toString();
 	}
 }
